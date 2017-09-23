7e29f4378ab85e9666fe1808f1471b4faa690481
Flávio Losada <flavio_gs96@hotmail.com>
2017-09-23 04:37:54 -0300

    conclusão da implementação, necessário verificar apenas os retornos de resultado para tela/console

--- CaixeiroViajante.m
+++ CaixeiroViajante.m
@@ -2,90 +2,108 @@ close all; clc; clear;
 
 global iag x y
 %% Configuracao Inicial
-cities=20; % numero das variaveis para serem otimizadas (genes)
-Nt=cities; % numero de colunas da matriz de populacao
+npar=20; % numero das variaveis para serem otimizadas (genes)
+Nt=npar; % numero de colunas da matriz de populacao
 
-%PontoX e pontoY de cada cidade - Posicao no plano.
-x=rand(1,cities);
-y=rand(1,cities); % cidades em (cidadeX, cidadeY)
-
-%Quem e a populacao?
-populacao = getPointsInPlane(cities);
-dist = cvfun(populacao);
-
-
-dist = sort(dist, 1)
-%positions = getPointsInPlane(cities);
-%X = positions(:,1);
-%Y = positions(:, 2);
-%Scatter exibe somente os pontos no plano
-%scatter(X, Y);
-% Hold on necessario para esperar a escrita do scatter
-%hold on;
-% Faz o plot aqui dos 2 primeiros elementos de cada vetor, 
-% correspondendo aos 2 primeiros pontos, criando a linha entre eles
-%plot(X(1:2), Y(1:2));
+x=rand(1,npar);
+y=rand(1,npar); % cidades em (cidadeX, cidadeY)
 
 % Adiciona o numero maximo de interacoes (Criterio de Parada)
+maxit = 1000;
 
 % Parametros do AG
 % tamanho da populacao
+tamPop = 20;
 % taxa de mutacao (probabilidade de 0.05)
 selecao=0.5; % fracao da populacao a ser mantida
-%manter=floor(selecao*tamPop); % numero de membros da populacao que sobrevivem
-%M=ceil((tamPop-manter)/2); % numero de cruzamentos
+manter=floor(selecao*tamPop); % numero de membros da populacao que sobrevivem
+M=ceil((tamPop-manter)/2); % numero de cruzamentos
 
 % calculo da probabilidade para a selecao dos pais
 probab=1; %probabilidade
-%for ii=2:manter
-%    probab=[probab ii*ones(1,ii)];
-%end
-%Nprobab=length(probab);
-%probab=manter-probab+1; 
+for ii=2:manter
+    probab=[probab ii*ones(1,ii)];
+end
+Nprobab=length(probab);
+probab=manter-probab+1; 
 
 
 % Gerar a populacao inicial
 iag=0; % contador para iniciar a geracao
 % gerar a populacao aleatoria (Veja funcao randperm)
 % deve ser uma matriz 20x20 (cromossomo + n. da populacao)
-
-%calcular o custo da populacao utilizando a funcao de aptidao
-% colocar o custo minimo no elemento 1 (Veja funcao sort)
-% organizar a populacao com o custo mais baixo primeiro
-% calcula o custo minimo da populacao (veja funcao min)
-% calcula a media aritmetica da populacao (veja funcao mean)
+populacao = funcaoGeraMatriz();
 
 
 %% Interacao pelas geracoes (LOOP PRINCIPAL)
-%while iag<maxit
-%    iag=iag+1; % incrementa o contador de geracoes
+while iag<maxit
+    iag=iag+1; % incrementa o contador de geracoes
     
-    % Escolha do Pai1 e Pa2 que sao escolhidos aleatoriamente do vetor
+    %calcular o custo da populacao utilizando a funcao de aptidao
+    populacao(:, 21) = cvfun(populacao);
+    % colocar o custo minimo no elemento 1 (Veja funcao sort)
+    % organizar a populacao com o custo mais baixo primeiro
+    populacao = sortrows(populacao, 21);
+    populacao = populacao(:, 1:20);
+    % calcula o custo minimo da populacao (veja funcao min)
+    % calcula a media aritmetica da populacao (veja funcao mean)
+    
+    % Escolha do Pai1 e Pa2 que são escolhidos aleatoriamente do vetor
     % probab
-    %escolha1=ceil(Nprobab*rand(1,M)); % escolher aleatoriamente na roleta os individuos
-    %escolha2=ceil(Nprobab*rand(1,M)); % escolher aleatoriamente na roleta os individuos
-    %indPai1=probab(escolha1); % selecionar os indices escolhidos na roleta para o pai 1
-    %indPai2=probab(escolha2); % selecionar os indices escolhidos na roleta para o pai 2
+    escolha1=ceil(Nprobab*rand(1,M)); % escolher aleatoriamente na roleta os indivíduos
+    escolha2=ceil(Nprobab*rand(1,M)); % escolher aleatoriamente na roleta os indivíduos
+    indPai1=probab(escolha1); % selecionar os indices escolhidos na roleta para o pai 1
+    indPai2=probab(escolha2); % selecionar os indices escolhidos na roleta para o pai 2
     
     % Execucao da Recombinacao (crossover)
-    %for ic=1:M
+    result = [];
+    for ic=1:M
         %seleciona o Pai 1
+        parents(1, :) = populacao(indPai1(1, ic), :);       
         %seleciona o Pai 2 
-    %end
+        parents(2, :) = populacao(indPai2(1, ic), :);       
         
-        % Faz a tecnica de recombinacao Cycle
-
-    % Faz a Mutacao da populacao
-
-   
-    % Se calcula um novo custo para a nova populacao
+        % Faz a técnica de recombinação Cycle
+        children = crossOver(parents);
+        result = vertcat(result, children);
+    end
+    populacao(11:20, :) = result;   
+    % Se calcula um novo custo para a nova população
     
     
     %_______________________________________________________
     % Organiza em ordem crescente os custos e associa aos parametros
 
-%end %iga
+end %iga
 
+%calcular o custo da populacao utilizando a funcao de aptidao
+populacao(:, 21) = cvfun(populacao);
+% colocar o custo minimo no elemento 1 (Veja funcao sort)
+% organizar a populacao com o custo mais baixo primeiro
+populacao = sortrows(populacao, 21);
 
 %_______________________________________________________
 % Mostrar os resultados
+scatter(x(populacao(1,1)), y(populacao(1,1)));
+hold on;
+scatter(x(populacao(1,20)), y(populacao(1,20)));
+hold on;
+plot(x(populacao(1,1:2)), y(populacao(1,1:2)), "color", "black", 
+     x(populacao(1,2:3)), y(populacao(1,2:3)), "color", "blue",
+     x(populacao(1,3:4)), y(populacao(1,3:4)), "color", "blue",
+     x(populacao(1,4:5)), y(populacao(1,4:5)), "color", "blue",
+     x(populacao(1,5:6)), y(populacao(1,5:6)), "color", "blue",
+     x(populacao(1,6:7)), y(populacao(1,6:7)), "color", "blue",
+     x(populacao(1,7:8)), y(populacao(1,7:8)), "color", "blue",
+     x(populacao(1,8:9)), y(populacao(1,8:9)), "color", "blue",
+     x(populacao(1,9:10)), y(populacao(1,9:10)), "color", "blue",
+     x(populacao(1,10:11)), y(populacao(1,10:11)), "color", "blue",
+     x(populacao(1,11:12)), y(populacao(1,11:12)), "color", "blue",
+     x(populacao(1,12:13)), y(populacao(1,12:13)), "color", "blue",
+     x(populacao(1,13:14)), y(populacao(1,13:14)), "color", "blue",
+     x(populacao(1,14:15)), y(populacao(1,14:15)), "color", "blue",
+     x(populacao(1,15:16)), y(populacao(1,15:16)), "color", "blue",
+     x(populacao(1,16:17)), y(populacao(1,16:17)), "color", "blue",
+     x(populacao(1,17:18)), y(populacao(1,17:18)), "color", "blue",
+     x(populacao(1,18:19)), y(populacao(1,18:19)), "color", "blue",
+     x(populacao(1,19:20)), y(populacao(1,19:20)), "color", "red");
--- crossOver.m
+++ crossOver.m
@@ -1,12 +1,16 @@
 function children = crossOver(parents)  
   
   children = parents;
+  %gera posição aleatória para trocar entre os cromossomos
   position = randi(20);  
   do
     hasRepeated = 0;
+    %troca os valores dos cromossomos na posicao escolhida
     valueChild = children(1, position);
     children(1, position) = children(2, position);
     children(2, position) = valueChild;
+    %verifica quais posições ficaram repetidas e retorne o loop para ajuste
+    %se necessário
     for i=1:20
       if (i != position && children(1, i) == children(1, position)) 
         position = i;
@@ -15,4 +19,14 @@ function children = crossOver(parents)
       end
     end
   until hasRepeated == 0;
+  %faz a mutação entre duas posições
+  for i=1:2
+    firstPosition = randi(20);
+    do
+      secondPosition = randi(20);
+    until secondPosition != firstPosition;
+    firstValue = children(i, firstPosition);
+    children(i, firstPosition) = children(i, secondPosition);
+    children(i, secondPosition) = firstValue;
+  end  
 end
